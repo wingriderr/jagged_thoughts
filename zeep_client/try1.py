b5212c10f7ca90ad4cc7bbce44521e80cd5c6b57
wingriderr <anwarnishil@gmail.com>
2021-09-04 12:23:34 +1000

    tweaked webservice url

--- zeep_client/try1.py
+++ zeep_client/try1.py
@@ -8,10 +8,11 @@ from zeep.wsse.username import UsernameToken
 def getClient(username,password):
     session = Session()
     session.auth = HTTPBasicAuth(username, password)
+    webstaging='https://ws.staging.training.gov.au/Deewr.Tga.Webservices/OrganisationServiceV8.svc?wsdl'
+    websandpit='https://ws.sandbox.training.gov.au/Deewr.Tga.WebServices/OrganisationServiceV2.svc?wsdl'
     #client = Client('https://ws.staging.training.gov.au/Deewr.Tga.Webservices/OrganisationServiceV8.svc?wsdl',
     #             transport=Transport(session=session))
-    client = Client(
-        'https://ws.staging.training.gov.au/Deewr.Tga.Webservices/OrganisationServiceV8.svc?wsdl',
+    client = Client(websandpit,
         wsse=UsernameToken(username, password))
     return client
 
@@ -19,7 +20,7 @@ def test_factory_namespace(client):
     string_array_type = client.get_type('ns1:OrganisationDetailsRequest')
     #node = client.create_message(client.service, 'ns1:OrganisationDetailsRequest', user='WebService.Read')
     request = string_array_type()
-    request.Code = 234
+    request.Code = 40735
     request.IncludeLegacyData = 0
     request.InformationRequested = [{
     "ShowCodes" : 1,
@@ -34,12 +35,13 @@ def test_factory_namespace(client):
     "ShowRestrictions" : 0,
     "ShowRtoClassifications" : 0,
     "ShowRtoDeliveryNotification" : 0,
-    "ShowTradingNames" : 0,
+    "ShowTradingNames" : 1,
     "ShowUrls" : 0
         }]
     print(request)
     obj=client.service.GetDetails(request)
-    #print(obj)
+    print(obj)
+    #this is very handy for eeryone to understand the wsdl structure
     #obj=client.wsdl.dump()
     #print(client.service.GetDetails())
     obj=1
@@ -57,7 +59,7 @@ def main():
     password ='Asdf098'
     client = getClient(user,password)
     org = test_factory_namespace(client)
-    #print(org)
+    print(org)
 
 if __name__ == "__main__":
     main()
